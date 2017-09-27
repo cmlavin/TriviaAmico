4d5b4803927830394ffdfa4771e4cc9d274e67f0
Catherine <cmlavin7@gmail.com>
2017-09-27 13:31:00 -0400

    Changed categories associated with a high score to text from API number.

--- src/components/HighScore.js
+++ src/components/HighScore.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Table } from 'semantic-ui-react'
+import categories from '../options/categories'
 
 const HighScore = (props) => {
+  const category = () => {
+    let foundCategory = categories.filter(obj => obj.value === props.category)
+    return foundCategory[0].text
+  }
+
   return(
       <Table.Row>
         <Table.Cell textAlign="center">{props.rank}</Table.Cell>
         <Table.Cell textAlign="center">{props.username}</Table.Cell>
-        <Table.Cell textAlign="center">{props.category}</Table.Cell>
+        <Table.Cell textAlign="center">{category()}</Table.Cell>
         <Table.Cell textAlign="center">{props.score}</Table.Cell>
       </Table.Row>
   )
--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -5,14 +5,13 @@ import { Icon, Table } from 'semantic-ui-react'
 class Leaderboard extends React.Component {
   componentWillReceiveProps(nextProps) {
     if(nextProps.scores.length !== 0 && nextProps.users.length !== 0 && nextProps.gameInfo.length !== 0) {
-      debugger
       nextProps.scores.map(score => score.username = (nextProps.users.filter(user => user.id === score.user_id)[0].username))
       nextProps.scores.map(score => score.category = (nextProps.gameInfo.filter(game => game.id === score.game_id)[0].category))
     }
   }
 
   sortedScores = () => {
-    if(this.props.users.length !== 0) {
+    if(this.props.users.length !== 0 && this.props.gameInfo.length !== 0) {
       let sorted = this.props.scores.sort(function(a, b) {return b.score - a.score}).slice(0, 10)
       return sorted.map((obj, i) => <HighScore key={i} rank={i+1} username={obj.username} score={obj.score} category={obj.category}/>)
     }
