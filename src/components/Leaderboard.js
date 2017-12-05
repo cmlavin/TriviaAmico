4be92b33b2fb1d3e9074a03024b9f38f5c9c71df
Catherine <cmlavin7@gmail.com>
2017-12-05 17:00:11 -0500

    Removed categories from leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -7,16 +7,17 @@ class Leaderboard extends React.Component {
 
   componentWillReceiveProps(nextProps) {
     if(nextProps.scores.length !== 0 && nextProps.users.length !== 0 && nextProps.gameInfo.length !== 0) {
+      //console.log(nextProps.scores)
       nextProps.scores.map(scoreObj => scoreObj.username = (nextProps.users.filter(user => user.id === scoreObj.user_id)[0].username))
-      nextProps.scores.map(scoreObj => scoreObj.category = (nextProps.gameInfo.filter(game => game.id === scoreObj.game_id)[0].category))
-      nextProps.scores.map(scoreObj => scoreObj.category = (categories.filter(catObj => catObj.value === scoreObj.category)[0].text))
+      //nextProps.scores.map(scoreObj => scoreObj.category = (nextProps.gameInfo.filter(game => game.id === scoreObj.game_id)[0].category))
+      //nextProps.scores.map(scoreObj => scoreObj.category = (categories.filter(catObj => catObj.value === scoreObj.category).text))
     }
   }
 
   sortedScores = () => {
     if(this.props.users.length > 0 && this.props.gameInfo.length > 0) {
       let sorted = this.props.scores.sort(function(a, b) {return b.score - a.score}).slice(0, 10)
-      return sorted.map((obj, i) => <HighScore key={i} rank={i+1} username={obj.username} score={obj.score} category={obj.category}/>)
+      return sorted.map((obj, i) => <HighScore key={i} rank={i+1} username={obj.username} score={obj.score} />)
     }
   }
 
@@ -33,7 +34,6 @@ class Leaderboard extends React.Component {
             <Table.Row>
               <Table.HeaderCell>Rank</Table.HeaderCell>
               <Table.HeaderCell>Username</Table.HeaderCell>
-              <Table.HeaderCell>Category</Table.HeaderCell>
               <Table.HeaderCell>High Score</Table.HeaderCell>
             </Table.Row>
           </Table.Header>
